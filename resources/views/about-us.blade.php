13146ac794b15698f9b8d04537b7f826a6c5a6a9
= <raulirawan33@gmail.com>
2024-07-25 11:53:17 +0700

    up

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -16,7 +16,7 @@
                                 <nav aria-label="breadcrumb ">
                                     <ol class="breadcrumb">
                                         <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                                        <li class="breadcrumb-item"><a href="#">Product</a></li>
+                                        <li class="breadcrumb-item"><a href="#">About Us</a></li>
                                     </ol>
                                 </nav>
                             </div>
